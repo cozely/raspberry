166336b33b5420915f53108840e4e1fa750d8a97
drakmaniso <moussault.laurent@gmail.com>
2018-12-21 09:17:25 +0100

    Tweak bcm package

--- bcm/bcm.go
+++ bcm/bcm.go
@@ -33,6 +33,29 @@ func GetDisplaySize(display uint16) (w, h int32, err error) {
 	return w, h, nil
 }
 
+// GetPeripheralAddress returns the ARM-side physical address where peripherals
+// are mapped.
+//
+// This is 0x20000000 on the Pi Zero, Pi Zero W, and the first generation of the
+// Raspberry Pi and Compute Module, and 0x3f000000 on the Pi 2, Pi 3 and Compute
+// Module 3.
 func GetPeripheralAddress() uintptr {
 	return uintptr(C.bcm_host_get_peripheral_address())
 }
+
+// GetPeripheralSize returns the size of the peripheral's space.
+//
+// Thi is 0x01000000 for all models.
+func GetPeripheralSize() uintptr {
+	return uintptr(C.bcm_host_get_peripheral_size())
+}
+
+// GetSDRAMAddress returns the bus address of the SDRAM.
+//
+// This is 0x40000000 on the Pi Zero, Pi Zero W, and the first generation of the
+// Raspberry Pi and Compute Module (GPU L2 cached), and 0xC0000000 on the Pi 2,
+// Pi 3 and Compute Module 3 (uncached).
+func GetSDRAMAddress() uintptr {
+	return uintptr(C.bcm_host_get_sdram_address())
+}
+
